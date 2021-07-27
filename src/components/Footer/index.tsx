0a7b2f29036c71bd9d2fc9849dee3614775445ee
jos3duardo <125362edu>
2021-07-27 12:18:08 -0400

    adicionando google adsense

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
     return (
         <Container className="footer mt-auto py-3 bg-dark">
                 <div className="container">
-                    <span className="text-muted">Todos os diretitos reservados <a href="https://jos3duardo.com.br/" target="_blank">Jos3duardo</a> {ano.getFullYear()}.</span>
+                    <span className="text-muted">Todos os direitos reservados <a href="https://jos3duardo.com.br/" target="_blank">Jos3duardo</a> {ano.getFullYear()}.</span>
                 </div>
         </Container>
     )
